c1e22560bd7dbc436671430021133016ccdfcce4
Hanna Hosk <hanna.hosk@gmail.com>
2024-04-29 13:59:49 +0200

    added Admin button to Navbar

--- app/components/navbar/Nav.tsx
+++ app/components/navbar/Nav.tsx
@@ -1,14 +1,24 @@
-import React from 'react';
-import styles from './nav.module.css';
+"use client";
+import React from "react";
+import styles from "./nav.module.css";
+import Link from "next/link";
+import { useRouter } from "next/navigation";
 
 const Nav = () => {
-	return (
-		<nav className={styles.nav}>
-			<ul>
-				<li>GRAPHER SHOT.</li>
-			</ul>
-		</nav>
-	);
+  const router = useRouter();
+  return (
+    <nav className={styles.nav}>
+      <span className={styles.nav_title}>GRAPHER SHOT.</span>
+      <button
+        onClick={() => {
+          router.push("/admin");
+        }}
+        className={styles.admin_btn}
+      >
+        ADMIN
+      </button>
+    </nav>
+  );
 };
 
 export default Nav;
